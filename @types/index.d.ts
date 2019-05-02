865c783268a89797d8e00f799cf2f46adb1afed2
Benny Megidish <bennymegk@gmail.com>
2019-05-02 14:14:08 +0300

    more typings for import options

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -24,3 +24,24 @@ export declare class Verifier {
      */
     static getInstitutionName(emailAddress: string): Promise<string>
 }
+
+/**
+ * check's if the email address is valid (in an proper email format - RFC 2822)
+ * @param {string} emailAddress email address to be checked
+ * @returns {boolean} true, if the email address is valid, false, otherwise
+ */
+export function isValidEmailAddress(emailAddress: string): boolean
+
+/**
+ * check's if the email address is academic
+ * @param {string} emailAddress email address to be checked
+ * @returns {Promise<boolean>} true, if the email address is academic, false, otherwise
+ */
+export function isAcademic(emailAddress: string): Promise<boolean>
+
+/**
+ * query email address institution name
+ * @param {string} emailAddress email address to be checked
+ * @returns {Promise<string>} academic institution name, if exist, '', otherwise
+ */
+export function getInstitutionName(emailAddress: string): Promise<string>
